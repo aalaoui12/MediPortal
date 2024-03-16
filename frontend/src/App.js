1c5a828f4445ee5592442a22c7617d99f07bc117
Ali Alaoui <aalaoui@Alis-MacBook-Pro-2.local>
2024-03-16 20:32:34 +0000

    Got Dynamic with Pimlico working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createConfig, WagmiProvider } from "wagmi";
+import { mainnet } from "viem/chains";
+import { http } from "viem";
+import { Pimlico } from "./Pimlico";
 
-function App() {
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [smartAccount, setSmartAccount] = useState(null);
+
+  const config = createConfig({
+    chains: [mainnet],
+    multiInjectedProviderDiscovery: false,
+    transports: {
+      [mainnet.id]: http(),
+    },
+  });
+
+  const queryClient = new QueryClient();
+
+  useEffect(() => {
+    if (smartAccount) console.log("Smart Account: ", smartAccount);
+  }, [smartAccount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DynamicContextProvider
+      settings={{
+        environmentId: process.env.REACT_APP_DYNAMIC_ENVIRONMENT_ID,
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>
+            <DynamicWidget />
+            <Pimlico
+              smartAccount={smartAccount}
+              setSmartAccount={setSmartAccount}
+            />
+          </DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </DynamicContextProvider>
   );
-}
+};
 
 export default App;
