d67fc4d9210f7ee55beba0c091b8d6c43e91c0b8
Ali Alaoui <aalaoui@Alis-MacBook-Pro-2.local>
2024-03-17 08:29:01 +0000

    Added more UI elements

--- src/app/MediPortal.tsx
+++ src/app/MediPortal.tsx
@@ -21,15 +21,19 @@ export default function MediPortal() {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="App">
-                <div className='flex flex-col space-y-2'>
+            <div className="App flex flex-col items-center">
+                <h1 className="text-3xl">
+                    MediPortal
+                </h1>
+                <div className='flex flex-col space-y-2 mt-8'>
+                    <button className="text-white bg-black rounded py-2 px-4">Sign in with wallet</button>
                     <IDKitWidget
                         signal={address}
                         action="mint_meditoken"
                         onSuccess={onSuccess}
                         app_id="app_staging_6987c97320a0eedbcbf943ce08898fd3"
                     >
-                        {({ open }) => <button onClick={open}>Verify with World ID</button>}
+                        {({ open }) => <button className="text-white bg-black rounded py-2 px-4" onClick={open}>Verify with World ID</button>}
                     </IDKitWidget>
                 </div>
             </div>
